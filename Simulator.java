76607f104afd8478b0b95466046d4e791ba91d4f
Rowlaner <edweirdo@live.co.uk>
2015-05-12 16:39:40 +0100

    Update Simulator.java

--- Simulator.java
+++ Simulator.java
@@ -1,28 +1,51 @@
-import java.util.ArrayList;
+import java.awt.Color;
 
-public class Simulator
-{
-  private ArrayList<Weed> weeds;
-  private ArrayList<BeanPlant> beanPlants;
-  private ArrayList<Farmer> farmers;
-  
-  // Constructor for the Simulator class, it initialises the private arraylists of actors
-  public Simulator()
-  {
-    weeds = new ArrayList<Weed>();
-    beanPlants = new ArrayList<BeanPlant>();
-    farmers = new ArrayList<Farmer>();
-  }
-  
-  // give each actor a random starting point
-  public void Populate()
-  {
-    // TODO..
-  }
-  
-  // iterates of populations of bean plants, weeds and farmers
-  public void SimulateOneStep(Field field, Field updateField)
-  {
-    // TODO...
-  }
+public class Simulator {
+	
+	private int step = 1;
+	
+	public Simulator(int depth, int width)
+	{
+		Field field = new Field(depth,width);
+		SimulatorView view = new SimulatorView(depth,width);
+		
+		view.setColor(Farmer.class, Color.black);
+		view.setColor(Weed.class, Color.blue);
+		view.setColor(BeanPlant.class, Color.green);
+		
+		populate(field);
+		
+		view.showStatus(step, field);
+	}
+	
+	public void populate(Field field)
+	{
+		field.clear();
+		int depth = field.getDepth();
+		int width = field.getWidth();
+		
+		for ( int x = 0 ; x >= width ; x++ ) {			// Goes through each row
+			for ( int y = 0 ; y >= depth ; y++ ) {		// Goes through each column
+				
+			}
+		}
+		
+		Farmer farmer = new Farmer();
+		Weed weed = new Weed();
+		BeanPlant beanPlant = new BeanPlant();
+		
+		field.place(farmer, 11, 40);
+		field.place(weed, 1, 12);
+		field.place(beanPlant, 49, 5);	
+		
+		field.clear();
+		field.place(farmer, 11, 40);
+		field.place(weed, 1, 12);
+		field.place(beanPlant, 49, 5);	
+	}
+	
+	public static void main(String[] args) 
+	{
+		Simulator simulator = new Simulator(50,50);
+	}	
 }
