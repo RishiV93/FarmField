10770baa6b686043d8518120b687cb4d123ce470
Rowlaner <edweirdo@live.co.uk>
2015-05-12 16:40:35 +0100

    Update Weed.java

--- Weed.java
+++ Weed.java
@@ -1,31 +1,9 @@
-public class Weed extends Actor
-{
-  // the age at which the Weed can begin spreading.
-  private static final int weedSpreadingAge = 20;
-  
-  Location location =  new Location();
-  
-  public Weed()
-  {
-    actor.actorType = ActorType.Weed.toString();
-  }
-  
-  public void grow (Field newField, Actor weed)
-  {
-    if (weed.getAge() >= weedSpreadingAge && weed.getAge() < plantMaximumAge)
-    {
-      Location newLoc = newField.freeAdjacentLocation(location);
-      
-      // only transfer if possible
-      if (newLoc == null)
-      {
-        setLocation(newLoc);
-        newField.place(this, newLoc);
-      }
-      else
-      {
-        weed.setAlive(false);
-      }
-    }
-  }
+
+public class Weed extends Plant {
+
+	public Weed()
+	{
+		float creationProbability = 0.1F;
+	}
+	
 }
