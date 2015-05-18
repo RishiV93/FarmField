c7d23da7a4ec42609ca71592bf72a808017b584f
Rowlaner <edweirdo@live.co.uk>
2015-05-18 09:25:42 +0100

    Update Weed.java

--- Weed.java
+++ Weed.java
@@ -1,9 +1,13 @@
-
-public class Weed extends Plant {
-
-	public Weed()
-	{
-		float creationProbability = 0.1F;
+public class Weed extends Actor {
+	
+	@Override
+	public void act(Field theField) {
+		// TODO Auto-generated method stub
+		System.out.println("WEED ACT");
 	}
 	
+	public Weed()
+	{
+		actorType = "Weed";
+	}
 }
