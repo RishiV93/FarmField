fbe0a9eb134df6becb23eed6d48d5f41e88cbb63
Rowlaner <edweirdo@live.co.uk>
2015-05-18 12:29:04 +0100

    Update BeanPlant.java

--- BeanPlant.java
+++ BeanPlant.java
@@ -1,10 +1,23 @@
-public class BeanPlant extends Actor {
+public class BeanPlant extends Actor 
+{
+	Location location = new Location(0,0);
 
 	@Override
 	public void act(Field theField) {
 		// TODO Auto-generated method stub
 		System.out.println("BEAN PLANT ACT");
-		
+	}
+	
+	@Override
+	public Location getLocation() {
+		// TODO Auto-generated method stub
+		return location;
+	}
+	
+	@Override
+	public void setLocation(Location sL) {
+		// TODO Auto-generated method stub
+		this.location = sL;
 	}
 	
 	public BeanPlant()
