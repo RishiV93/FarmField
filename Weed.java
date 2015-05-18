0231f88c62d5dcb149eb982d079f0ba1b85d85a6
Rowlaner <edweirdo@live.co.uk>
2015-05-18 12:28:44 +0100

    Update Weed.java

--- Weed.java
+++ Weed.java
@@ -1,9 +1,24 @@
-public class Weed extends Actor {
+public class Weed extends Actor
+{
+	Location location = new Location(0,0);
+
 	
 	@Override
 	public void act(Field theField) {
 		// TODO Auto-generated method stub
 		System.out.println("WEED ACT");
+	}
+	
+	@Override
+	public Location getLocation() {
+		// TODO Auto-generated method stub
+		return location;
+	}
+	
+	@Override
+	public void setLocation(Location sL) {
+		// TODO Auto-generated method stub
+		this.location = sL;
 	}
 	
 	public Weed()
