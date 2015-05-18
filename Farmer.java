71cb4dc7e07a4d6556c8c929e63a592752eef8fa
Rowlaner <edweirdo@live.co.uk>
2015-05-18 09:23:39 +0100

    Update Farmer.java

--- Farmer.java
+++ Farmer.java
@@ -1,9 +1,30 @@
+public class Farmer extends Actor 
+{
+	  Location location = new Location(0,0);
 
-public class Farmer extends Actor {
-
-	public Farmer()
-	{
-		float creationProbability = 0.1F;
-	}
+	  public void farmerAct(Field theField)
+	  {
+		  System.out.println(location);
+	  }
 	
+	  Farmer()
+	  {
+		  actorType = "Farmer";
+	  }
+	  
+	  public void setLocation(Location sL)
+	  {
+		  this.location = sL;
+	  }
+	  
+	  public Location getLocation()
+	  {
+		  return location;
+	  }
+	  
+	  @Override
+	public void act(Field theField) {
+		// TODO Auto-generated method stub
+		System.out.println("FARMER ACT");
+	}
 }
