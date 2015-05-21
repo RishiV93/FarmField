bfdf7e00270488aaeea3848767bd070c84d2aeeb
Rishi Verma <r_verma2002@hotmail.com>
2015-05-21 20:11:44 +0100

    Update BeanPlant.java

--- BeanPlant.java
+++ BeanPlant.java
@@ -1,27 +1,32 @@
 public class BeanPlant extends Actor 
 {
-	Location location = new Location(0,0);
+    Location location = new Location(0,0);
 
-	@Override
-	public void act(Field theField) {
-		// TODO Auto-generated method stub
-		System.out.println("BEAN PLANT ACT");
-	}
-	
-	@Override
-	public Location getLocation() {
-		// TODO Auto-generated method stub
-		return location;
-	}
-	
-	@Override
-	public void setLocation(Location sL) {
-		// TODO Auto-generated method stub
-		this.location = sL;
-	}
-	
-	public BeanPlant()
-	{
-		actorType = "BeanPlant";
-	}
+    @Override
+    public void act(Field theField) 
+    {
+        // TODO Auto-generated method stub
+        System.out.println("BEAN PLANT ACT");
+        
+        DoPlantAction();
+    }
+
+    @Override
+    public Location getLocation() 
+    {
+        // TODO Auto-generated method stub
+        return location;
+    }
+
+    @Override
+    public void setLocation(Location sL) 
+    {
+        // TODO Auto-generated method stub
+        this.location = sL;
+    }
+
+    public BeanPlant()
+    {
+        actorType = "BeanPlant";
+    }
 }
