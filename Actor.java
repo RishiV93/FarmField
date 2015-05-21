140393a2eaaed5c93df5533e2f4e0dd781c9a2cf
Rishi Verma <r_verma2002@hotmail.com>
2015-05-21 20:11:08 +0100

    Update Actor.java
    Added new methods which will be used when the plant actors are taking their steps

--- Actor.java
+++ Actor.java
@@ -1,35 +1,72 @@
 public abstract class Actor extends ModelConstants
 {
-  private int age;
-  private boolean alive = true;
-  public String actorType;
-  public static final int plantMaximumAge = 150;
+    private int age;
+    private boolean alive = true;
+    public String actorType;
+    public static final int plantMaximumAge = 150;
   
-  // (row, column)
-  private Location location;
+    // (row, column)
+    private Location location;
 
-  // the various types of actors within the system.
-  public  enum  ActorType
-  {
-    BeanPlant,
-    Weed,
-    Farmer
-  }
-  
-  public abstract void act(Field theField);
-
-  public abstract void setLocation(Location location);
-  
-  public abstract Location getLocation();
-  
-  
-
-  // updates the alive indicator for weeds and beanplant
-  public void UpdateActorAliveStatus(boolean newStatus)
-  {
-    if(actorType == "BeanPlant" || actorType == "Weed")
+    // the various types of actors within the system.
+    public  enum  ActorType
     {
-      alive = newStatus;  
+        BeanPlant,
+        Weed,
+        Farmer
     }
-  }
+
+    public abstract void act(Field theField);
+
+    public abstract void setLocation(Location location);
+  
+    public abstract Location getLocation();
+  
+    // returns bool true if the actor is a type of plant, else false.
+    public boolean IsPlant()
+    {
+        if(actorType == "BeanPlant" || actorType == "Weed")
+        {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    // updates the alive indicator for weeds and beanplant to flase if they are determined to be dead.
+    public void SetActorAliveStatus()
+    {
+        if(IsPlant())
+        {
+            alive = false;  
+        }
+    }
+    
+    // increments the age of an actor (beanplant and weeds only) which will be called upon the use of the Act method in derived classes.
+    public void IncreaseAge()
+    {
+        this.age = (this.age + 1);
+    }
+    
+    // returns the act of the actor.
+    public int GetAge()
+    {
+        return this.age;
+    }
+    
+    // to be called within the derived Beanplant and weed classes to do their action which occurs per step.
+    public void DoPlantAction()
+    {
+        if (IsPlant()) 
+        {
+            IncreaseAge();
+        
+            if (GetAge() >= ModelConstants.MAXIMUM_AGE) 
+            {
+                SetActorAliveStatus();
+            
+                // remove from JFrame
+            }    
+        }        
+    }
 }
