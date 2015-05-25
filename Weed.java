064ce096aab1379b87345ba045b189448f755977
Rishi Verma <r_verma2002@hotmail.com>
2015-05-25 10:45:52 +0100

    Update Weed.java

--- Weed.java
+++ Weed.java
@@ -1,31 +1,18 @@
 public class Weed extends Actor
 {
-	Location location = new Location(0,0);
+    Location location = new Location(0,0);
 
-	
-	@Override
-	    public void act(Field theField) 
+    @Override
+    public void act(Field theField) 
     {
         // TODO Auto-generated method stub
         System.out.println("WEED ACT");
         
         DoPlantAction();
     }
-	
-	@Override
-	public Location getLocation() {
-		// TODO Auto-generated method stub
-		return location;
-	}
-	
-	@Override
-	public void setLocation(Location sL) {
-		// TODO Auto-generated method stub
-		this.location = sL;
-	}
-	
-	public Weed()
-	{
-		actorType = "Weed";
-	}
+
+    public Weed()
+    {
+        actorType = "Weed";
+    }
 }
