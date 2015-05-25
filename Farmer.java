a29e4b4387e584ad0fac91825a8167796a56e728
Rishi Verma <r_verma2002@hotmail.com>
2015-05-25 10:45:00 +0100

    Update Farmer.java

--- Farmer.java
+++ Farmer.java
@@ -1,30 +1,21 @@
 public class Farmer extends Actor 
 {
 	  Location location = new Location(0,0);
-	
-	  Farmer()
-	  {
-		  actorType = "Farmer";
-	  }
-	  
-	  public void setLocation(Location sL)
-	  {
-		  this.location = sL;
-	  }
-	  
-	  public Location getLocation()
-	  {
-		  return location;
-	  }
-	  
-	  @Override
-	public void act(Field theField) {
-		// TODO Auto-generated method stub
-		System.out.println("FARMER ACT");
-		
-		// MOVEMENT
-		theField.freeAdjacentLocation(location);
-		System.out.println(theField.freeAdjacentLocation(location));
-		theField.adjacentLocations(location);
-	}
+
+    Farmer()
+    {
+        actorType = "Farmer";
+    }
+
+    @Override
+    public void act(Field theField) 
+    {
+        // TODO Auto-generated method stub
+        System.out.println("FARMER ACT");
+
+        // MOVEMENT
+        theField.freeAdjacentLocation(location);
+        System.out.println(theField.freeAdjacentLocation(location));
+        theField.adjacentLocations(location);
+    }
 }
