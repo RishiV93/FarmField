91192668c6d1ce4b18ad64c2e3999cde201be102
Rishi Verma <r_verma2002@hotmail.com>
2015-06-11 14:08:38 +0100

    Update BeanPlant.java

--- BeanPlant.java
+++ BeanPlant.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
+
 public class BeanPlant extends Actor 
 {
     Location location = new Location(0,0);
 
     @Override
-    public void act(Field theField) 
+    public void act(Field theField, Simulator simulator) 
     {
         // TODO Auto-generated method stub
         //System.out.println("BEAN PLANT ACT");
         
         DoPlantAction();
+    }
+    
+    @Override
+    public void act(Field theField, ArrayList<Actor> actors) 
+    {
+    	//MoveFarmer(theField);
+    	
+    	//DealWithWeeds(theField);
+    	
+    	//PlantBeanplant(theField);        
     }
 
     public BeanPlant()
